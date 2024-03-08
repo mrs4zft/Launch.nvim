4e56d92c5b2ad9af24ee58f1bfc1da2c6fa11d8d
Markus Scholz <marsch@1q84.fritz.box>
2024-03-08 23:01:46 +0100

    Adds trouble.

---
+++ lua/user/extras/trouble.lua
@@ -0,0 +1,25 @@
+local M = {
+ "folke/trouble.nvim",
+ dependencies = { "nvim-tree/nvim-web-devicons" },
+ opts = {
+  -- your configuration comes here
+  -- or leave it empty to use the default settings
+  -- refer to the configuration section below
+ },
+}
+
+function M.config()
+  local wk = require("which-key")
+  -- Lua
+  wk.register({
+    ["<leader>d"] = { name = 'Trouble diagnostics'},
+    ["<leader>dx"] = {function() require("trouble").toggle() end, 'Open trouble' },
+    ["<leader>dw"] = {function() require("trouble").toggle("workspace_diagnostics") end, 'Workspace diagnostics'},
+    ["<leader>dd"] = {function() require("trouble").toggle("document_diagnostics") end, 'Document diagnostics'},
+    ["<leader>dq"] = {function() require("trouble").toggle("quickfix") end, 'Trouble quickfix'},
+    ["<leader>dl"] = {function() require("trouble").toggle("loclist") end, 'Loclist'},
+    ["<leader>dR"] = {function() require("trouble").toggle("lsp_references") end, 'LSP references'},
+  })
+end
+
+return M
