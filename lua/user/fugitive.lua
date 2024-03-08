76a43b276bd9ffa0d87ce2f4ed41d56ec1de69e6
Markus Scholz <marsch@1q84.fritz.box>
2024-03-08 23:00:58 +0100

    Adds fugitive.

--- lua/user/fugitive.lua
+++ lua/user/fugitive.lua
@@ -5,7 +5,7 @@ local M = {
 function M.config()
   local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
   local autocmd = vim.api.nvim_create_autocmd
-  vim.keymap.set("n", "<leader>gf", vim.cmd.Git)
+  vim.keymap.set("n", "<leader>gf", vim.cmd.Git, { desc = "Fugitive" })
 
   autocmd("BufWinEnter", {
     group = ThePrimeagen_Fugitive,
@@ -17,18 +17,21 @@ function M.config()
 
       local bufnr = vim.api.nvim_get_current_buf()
       local opts = { buffer = bufnr, remap = false }
+      opts.desc = 'Git push'
       vim.keymap.set("n", "<leader>p", function()
         vim.cmd.Git('push')
       end, opts)
 
       -- rebase always
+      opts.desc = 'Git pull rebase'
       vim.keymap.set("n", "<leader>P", function()
         vim.cmd.Git({ 'pull', '--rebase' })
       end, opts)
 
-      -- NOTE: It allows me to easily set the branch i am pushing and any tracking
-      -- needed if i did not set the branch up correctly
-      vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
+    -- NOTE: It allows me to easily set the branch i am pushing and any tracking
+    -- needed if i did not set the branch up correctly
+    opts.desc = 'Push selecting origin'
+    vim.keymap.set("n", "<leader>gt", ":Git push -u origin ", opts)
     end
   })
 
