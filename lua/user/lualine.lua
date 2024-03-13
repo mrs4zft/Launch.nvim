c2a0201594ba2c9b9e6cb4e68bdc513e5e715e7e
Markus Scholz <marsch@1q84.fritz.box>
2024-03-13 23:33:03 +0100

    Alters lualine.

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -8,19 +8,21 @@ local M = {
 function M.config()
   require("lualine").setup {
     options = {
-      component_separators = { left = "", right = "" },
-      section_separators = { left = "", right = "" },
+      -- component_separators = { left = "", right = "" },
+      -- section_separators = { left = "", right = "" }, 
+      component_separators = { left = '', right = ''},
+      section_separators = { left = '', right = ''},
       ignore_focus = { "NvimTree" },
     },
     sections = {
       lualine_a = {},
-      lualine_b = { "branch" },
+      lualine_b = { "branch" , "diff"},
       lualine_c = { "diagnostics" },
       lualine_x = {  "filetype" },
       lualine_y = { "progress" },
-      lualine_z = {},
+      lualine_z = {"location"},
     },
-    extensions = { "quickfix", "man", "fugitive" },
+    extensions = { "quickfix", "man", "fugitive" , "trouble", "nvim-dap-ui", "oil"},
   }
 end
 
